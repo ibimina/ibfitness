93aaf073c3369eb9c4d43ec291c3352cd58187f1
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-10-09 18:29:21 +0100

    Added Routes to application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home/Home";
+// import Home from "./pages/home/Home";
+import { Landing } from "./pages/landing/Landing";
+import Login from "./pages/landing/Login";
+import Signup from "./pages/landing/Signup";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup/>} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
