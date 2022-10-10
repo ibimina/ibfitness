3bc4646a907a7dccfa6951d2f9267eef4e20b952
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-10-10 12:54:05 +0100

    Added firebase create user function to application

--- src/pages/landing/Signup.js
+++ src/pages/landing/Signup.js
@@ -2,7 +2,41 @@ import { Footer } from "../../components/Footer";
 import { NavBar } from "../../components/NavBar";
 
 
+import { auth } from "../../firebase/config";
+import { createUserWithEmailAndPassword,updateProfile } from "firebase/auth";
+import useAuthContext from "../../hooks/useAuthContext";
+import { useState } from "react";
+
+
+
 export default function Signup () {
+  const[registerUser,setRegisterUser]=useState(false)
+  const [registerError,setRegisterError]=useState(null)
+const{dispatch}=useAuthContext()
+const handleSubmit= async(e)=>{
+  try {
+    setRegisterUser(true)
+    setRegisterError(false)
+    e.preventDefault();
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      e.target.email.value,
+      e.target.password.value
+    );
+      updateProfile(userCredential.user, {
+        displayName: e.target.username.value,
+      });
+    dispatch({ type: "LOGIN", user: userCredential.user });
+ console.log(userCredential.user);
+    setRegisterUser(false)
+    setRegisterError(null)
+    e.target.reset()
+  } catch (error) {
+    setRegisterUser(false)
+    setRegisterError("user already exist");
+  }
+ 
+} 
 
     return (
       <>
@@ -11,20 +45,26 @@ export default function Signup () {
           Hey there, <span className="wlc">create an account</span>
         </p>
         <div className="login-pg">
-          <form>
+          <form onSubmit={handleSubmit}>
             <label>
               <span>Enter Email</span>
-              <input type="email" placeholder="Email" />
+              <input type="email" placeholder="Email" name="email" />
             </label>
             <label>
               <span>Enter Username</span>
-              <input type="text" placeholder="Username" />
+              <input type="text" placeholder="Username" name="username" />
             </label>
             <label>
               <span>Enter password</span>
-              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="Password" name="password" />
             </label>
-            <input type="submit" value="Register" className="submit" />
+            {!registerUser && (
+              <input type="submit" value="Register" className="submit" />
+            )}
+            {registerUser && (
+              <input type="submit" value="Loading" disabled className="submit" />
+            )}
+            {registerError && <>{registerError}</>}
           </form>
         </div>
 
