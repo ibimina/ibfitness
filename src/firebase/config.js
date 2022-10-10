9381873544966a00c6b09a312d7d23a597c34c30
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-10-10 12:52:14 +0100

    Added firebase configuration

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALKy93338Yt0EH0NZWXNZPrzgVvSZtl94",
+  authDomain: "ib-fitness.firebaseapp.com",
+  projectId: "ib-fitness",
+  storageBucket: "ib-fitness.appspot.com",
+  messagingSenderId: "556162610219",
+  appId: "1:556162610219:web:18be48963b9afa027e266e"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+const auth = getAuth()
+export {auth}
