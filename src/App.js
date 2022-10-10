c550e89f4a7edb3072149a0e07d92cea3fb34257
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-10-10 12:59:43 +0100

    wrapped app component with authcontextprovided

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { AuthContextProvider } from "./context/AuthContext";
 // import Home from "./pages/home/Home";
 import { Landing } from "./pages/landing/Landing";
 import Login from "./pages/landing/Login";
@@ -8,13 +9,15 @@ import Signup from "./pages/landing/Signup";
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup/>} />
-        </Routes>
-      </div>
+      <AuthContextProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
