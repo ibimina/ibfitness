d0e7d5fbe39c7ce4b938796efe766b8dbb39995d
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-10-10 12:58:46 +0100

    Added React Createcontext and useReducer hook

---
+++ src/context/AuthContext.js
@@ -0,0 +1,39 @@
+import { createContext,useEffect,useReducer } from "react";
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import {auth} from "../firebase/config"
+import { onAuthStateChanged } from "firebase/auth";
+
+export const AuthContext = createContext();
+
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "AUTHREADY":
+      return { user: action.payload, authChanged: true };
+    default:
+      return state;
+  }
+};
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+    authChanged: false,
+  });
+ useEffect(()=>{
+const unsub =onAuthStateChanged(auth, (user) => {
+dispatch({type:"AUTHREADY",payload:user})
+unsub()
+});
+
+
+ },[])
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
