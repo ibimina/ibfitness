14ef8b46a13b1a02b8e215b0ace4f3f5e8c8162e
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-10-13 23:07:53 +0100

    refactored code

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,27 +1,10 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+
 
 export default function Home() {
   return (
     <div>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <NavLink></NavLink>
-            </li>
-            <li>
-              <NavLink></NavLink>
-            </li>
-            <li>
-              <NavLink></NavLink>
-            </li>
-            <li>
-              <NavLink></NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <h1>hello</h1>
+        </div>
   );
 }
