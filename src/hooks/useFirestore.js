63cab7d4c0e906259947a434aa1f297d97f995ff
ibimina <96787210+ibimina@users.noreply.github.com>
2022-10-15 12:07:58 +0100

    Added an update_doc switch case and also adduserdocument, updateDocument,unsub function to hook.

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,6 +1,7 @@
-import { collection, addDoc } from "firebase/firestore";
-import { useReducer } from "react";
+import { collection, addDoc, updateDoc, doc, setDoc } from "firebase/firestore";
+import { useEffect, useReducer, useState } from "react";
 import { db } from "../firebase/config";
+
 const firestoreReducer = (state, action) => {
   switch (action.type) {
     case "LOADING":
@@ -8,6 +9,13 @@ const firestoreReducer = (state, action) => {
     case "ERROR":
       return { loading: false, error: true, success: true, document: null };
     case "ADD_DOC":
+      return {
+        loading: false,
+        error: null,
+        document: action.payload,
+        success: true,
+      };
+    case "UPDATE_DOC":
       return {
         loading: false,
         error: null,
@@ -22,7 +30,8 @@ const firestoreReducer = (state, action) => {
   }
 };
 
-export const UseFirestore = (c) => {
+export const UseFirestore = (c, q) => {
+  const [dispatchCancelled, setDispatchCancelled] = useState(false);
   const [response, dispatch] = useReducer(firestoreReducer, {
     document: null,
     isloading: false,
@@ -30,20 +39,47 @@ export const UseFirestore = (c) => {
     success: null,
   });
 
-  const docRef = collection(db, c);
-
-  const addDocument = async (data) => {
+  let docRef = collection(db, c);
+  const dispatchIsNotCancelled = (action) => {
+    if (dispatchCancelled) {
+      dispatch(action);
+    }
+  };
+  const addUserDocument = async (data) => {
+    dispatch({ type: "LOADING" });
     try {
-      const doc = await addDoc(docRef, { data });
-      dispatch({type:"ADD_DOC",payload:addDocument})
-      console.log(response);
-      console.log("Document written with ID: ", doc.id);
-      
+      const doct = await setDoc(doc(db, c, data.uid), data);
+      dispatchIsNotCancelled({ type: "ADD_DOC", payload: doct });
     } catch (e) {
       console.error("Error adding document: ", e);
-       dispatch({ type: "ERROR", payload: "Could not add" });
+      dispatchIsNotCancelled({ type: "ERROR", payload: "Could not add" });
     }
   };
 
-  return { addDocument ,response};
+  const addDocument = async (data) => {
+    dispatch({ type: "LOADING" });
+    try {
+      const doc = await addDoc(docRef, { data });
+      dispatchIsNotCancelled({ type: "ADD_DOC", payload: doc });
+      console.log("Document written with ID: ", doc.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      dispatchIsNotCancelled({ type: "ERROR", payload: "Could not add" });
+    }
+  };
+  const upDateDocument = async (data) => {
+    dispatch({ type: "LOADING" });
+    try {
+      const doct = await updateDoc(doc(db, c, q), data);
+      dispatchIsNotCancelled({ type: "UPDATE_DOC", payload: doct });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      dispatchIsNotCancelled({ type: "ERROR", payload: "Could not update" });
+    }
+  };
+
+  useEffect(() => {
+    return () => setDispatchCancelled(true);
+  }, []);
+  return { addDocument, upDateDocument, response, addUserDocument };
 };
